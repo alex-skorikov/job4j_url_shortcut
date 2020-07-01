1dc7ee0237b7b827731e7e8198ff13f0f6a73d22
alex-skorikov <the.na4ig@gmail.com>
2020-07-02 01:44:06 +1000

    Added test statistic controller.

--- src/test/java/com/skorikov/job4jurlshortcut/controller/StatisticControllerTest.java
+++ src/test/java/com/skorikov/job4jurlshortcut/controller/StatisticControllerTest.java
@@ -12,12 +12,13 @@ import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -27,27 +28,34 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @Sql(value = {"/create_site_before.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
 @Sql(value = {"/create_site_after.sql"}, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 @WithUserDetails(value = "site_1")
-class ConvertControllerTest {
+class StatisticControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
     @Autowired
-    private ConvertController convertController;
+    private StatisticController statisticController;
 
     @Test
     void controllerIsNotNull() {
-        assertThat(convertController).isNotNull();
+        assertThat(statisticController).isNotNull();
     }
 
     @Test
-    void testConvertControllerGetLinkCode() throws Exception {
-        this.mockMvc.perform(post("/convert").contentType(MediaType.APPLICATION_JSON)
-                .content("{\"url\" : \"testLink\"}"))
+    void testRedirect() throws Exception {
+        this.mockMvc.perform(get("/redirect/").contentType(MediaType.APPLICATION_JSON)
+                .param("code", "IreGCc"))
                 .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.code").isNotEmpty());
+                .andExpect(header().string("REDIRECT", "link/1"))
+                .andExpect(status().is(302));
     }
 
-
+    @Test
+    void testGetStatistic() throws Exception {
+        this.mockMvc.perform(post("/statistic").contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(jsonPath("$[0].total").value(3))
+                .andExpect(jsonPath("$[0].url").value("link/1"))
+                .andExpect(status().isOk());
+    }
 }

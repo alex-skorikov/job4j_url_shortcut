d757ff1d073a919666b8ffd5a30fb7a1f8e0b011
alex-skorikov <the.na4ig@gmail.com>
2020-07-01 22:01:40 +1000

    Added test convert controller.

---
+++ src/test/java/com/skorikov/job4jurlshortcut/controller/ConvertControllerTest.java
@@ -0,0 +1,57 @@
+package com.skorikov.job4jurlshortcut.controller;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+
+@AutoConfigureMockMvc
+@TestPropertySource("/application.test.properties")
+@Sql(value = {"/create_site_before.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(value = {"/create_site_after.sql"}, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+@WithUserDetails(value = "site_1")
+class ConvertControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ConvertController convertController;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @Test
+    void controllerIsNotNull() {
+        assertThat(convertController).isNotNull();
+    }
+
+    @Test
+    void testConvertControllerGetLinkCode() throws Exception {
+        this.mockMvc.perform(post("/convert").contentType(MediaType.APPLICATION_JSON)
+                .content("{\"url\" : \"testLink\"}"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.code").isNotEmpty());
+    }
+
+
+}
--- src/test/resources/create_site_before.sql
+++ src/test/resources/create_site_before.sql
@@ -6,5 +6,5 @@ insert into sites(id, site_url, password, status) values
 (12, 'site_2', '$2a$08$fNUHI3FnO3cbT6VAcClJOOsIq93f2101ud2RAKiZFAh7Y2h.oFRzC', 'ACTIVE');
 
 insert into links(id, link, code, status, vizit_count, site_id) values
-(1, 'link/1', 'IreGCc', 'ACTIVE', 3, 11),
-(2, 'link/2', 'X5vumU' , 'ACTIVE', 5, 12);
+(11, 'link/1', 'IreGCc', 'ACTIVE', 3, 11),
+(12, 'link/2', 'X5vumU' , 'ACTIVE', 5, 12);
